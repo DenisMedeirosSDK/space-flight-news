aa57f1b03fd9ba862fc04e4d3e94b871559921da
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-02-22 18:09:30 -0300

    feat: add documentation open api

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -5,6 +5,9 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 import './container';
 import { routes } from './routes';
 
@@ -15,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes);
 
 export { app };
