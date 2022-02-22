c208000b647f854780762e221ac7290c2b6dec75
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-02-22 18:49:17 -0300

    feat: add express async errors

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import helmet from 'helmet';
 import cors from 'cors';
 
@@ -15,10 +16,25 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
 app.use(helmet());
+app.use(cors({ origin: '*' }));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: 'Internal server error',
+    });
+  }
+);
 
 export { app };
