f6e59aaed584bc44a8b3b66499e5bef11524c1b0
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-02-24 10:27:20 -0300

    feat: add Sentry monitor log errors

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -6,6 +6,8 @@ import 'express-async-errors';
 import helmet from 'helmet';
 import cors from 'cors';
 import { errors as celebrateErrors } from 'celebrate';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
@@ -17,6 +19,25 @@ import { startJob } from '../articles/job';
 
 mongoConnection();
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN as string,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({
+      // to trace all requests to the default router
+      app,
+      // alternatively, you can specify the routes you want to trace:
+      // router: someRouter,
+    }),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 app.use(express.json());
 
@@ -43,4 +64,17 @@ app.use(
   }
 );
 
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 404 and 500 errors
+      if (error.status === 404 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 export { app };
