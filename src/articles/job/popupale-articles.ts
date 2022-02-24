16f4aa3b573388881817bdfb89725ac4b4de7d36
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-02-24 18:01:32 -0300

    fix: populate DB articles incremental by id

--- src/articles/job/popupale-articles.ts
+++ src/articles/job/popupale-articles.ts
@@ -1,37 +1,51 @@
 import axios from 'axios';
 import { scheduleJob } from 'node-schedule';
-
 import { client } from '../../infra/database/mongodb';
 
 async function execute() {
   let limit = 1000;
   let start = 0;
-  let running = true;
   let articles = [];
-  let page = start++;
 
-  try {
-    while (running) {
-      const response = await axios.get(
-        `https://api.spaceflightnewsapi.net/v3/articles`,
-        {
-          params: {
-            _limit: limit,
-            _start: page,
-            _sort: 'id',
-          },
-        }
-      );
+  const getCountDatabase = await client
+    .db('coodesh')
+    .collection('articles')
+    .estimatedDocumentCount();
 
-      articles = response.data;
+  start = getCountDatabase;
 
-      client.db('coodesh').collection('articles').insertMany(articles);
-    }
-  } catch (error) {
-    running = false;
+  if (start === 0) {
+    const response = await axios.get(
+      `https://api.spaceflightnewsapi.net/v3/articles`,
+      {
+        params: {
+          _limit: limit,
+          _start: start,
+          _sort: 'id',
+        },
+      }
+    );
+    articles = response.data;
+    await client.db('coodesh').collection('articles').insertMany(articles);
+  }
+
+  if (start > 0) {
+    const response = await axios.get(
+      `https://api.spaceflightnewsapi.net/v3/articles`,
+      {
+        params: {
+          _limit: limit,
+          _start: start++,
+          _sort: 'id',
+        },
+      }
+    );
+
+    articles = response.data;
+    await client.db('coodesh').collection('articles').insertMany(articles);
   }
 }
 
-export const populateArticles = scheduleJob('* * * * * *', () => {
+export const populateArticles = scheduleJob('*/10 * * * * *', () => {
   execute();
 });
